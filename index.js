fc21b159a75bb6459c394c7ec335e11673ac7d9e
tpwiedy <wiedy.pratama@gmail.com>
2022-06-05 17:36:43 +0700

    feat: challenge 5 Done

--- index.js
+++ index.js
@@ -6,15 +6,35 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, fnSort) => {
+  //panggil sorter function
+  sortData = fnSort(arr);
+
+  //membuat list number dari sorted data
+  let data = [];
+  for (let i = 0; i < sortData.length; i++) {
+    //looping data
+    let loop = "";
+    loop += i + 1 + ". " + sortData[i];
+    //menambahkan data ke array
+    data.push(loop);
+  }
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+  let data = arr.sort();
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  let data = arr.sort().reverse();
+  return data;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
